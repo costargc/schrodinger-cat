30e2c559c71a416842d6ea43ef98de1aa3e963cf
Rodrigo Costa <costa.rgc@gmail.com>
2019-05-18 13:05:55 -0400

    gif

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
   $("#cat-button").on("click", function() {
 
     // Storing our giphy API URL for a random cat image
-    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=loWyj00bILUWgGTJBzXxba03ZaupxhcG&tag=cats&limit=1";
+    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=N0cWjMKmQwmVr5KxTdt6JhNyOAJtwzGw&tag=cats&limit=1";
 
     // Perfoming an AJAX GET request to our queryURL
     $.ajax({
